2d09a12060edee231e823c9504b81a18cc7c434a
Sébastien Lehmstedt <sebastien.lehmstedt@odq.qc.ca>
2024-02-24 17:56:12 -0500

    position struct and texture loading

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,64 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
+use std::path::Path;
+
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
+use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
+use piston::window::*;
 use piston::input::{ButtonArgs, ButtonState, Button, Key, ButtonEvent};
+
+pub struct Position {
+    x: f64,
+    y: f64
+}
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    posX: f64,
-    posY: f64,
-    speedX: f64,
-    speedY: f64
+    position: Position,
+    speed: Position,
+    map: Texture,
+    character: Texture
 }
+
+const SPEED: f64 = 50.0;
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-
         self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
+
+            clear([1.0, 1.0, 1.0, 1.0], gl);
 
             let transform = c
                 .transform
-                .trans(self.posX, self.posY)
-                .trans(-25.0, -25.0);
+                .trans(self.position.x, self.position.y)
+                .scale(0.5, 0.5);
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            image(&self.map, c.transform, gl);
+
+            image(&self.character, transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        self.posX += self.speedX;
-        self.posY += self.speedY;
+        self.position.x += self.speed.x * args.dt;
+        self.position.y += self.speed.y * args.dt;
     }
 
     fn change_direction(&mut self, args: &ButtonArgs){
-        self.speedX = 0.0;
-        self.speedY = 0.0;
+        self.speed.x = 0.0;
+        self.speed.y = 0.0;
 
         if args.state == ButtonState::Press {
             match args.button {
-                Button::Keyboard(Key::Up) => self.speedY -= 1.0,
-                Button::Keyboard(Key::Down) => self.speedY += 1.0,
-                Button::Keyboard(Key::Left) => self.speedX -= 1.0,
-                Button::Keyboard(Key::Right) => self.speedX += 1.0,
+                Button::Keyboard(Key::Up) => self.speed.y -= SPEED,
+                Button::Keyboard(Key::Down) => self.speed.y += SPEED,
+                Button::Keyboard(Key::Left) => self.speed.x -= SPEED,
+                Button::Keyboard(Key::Right) => self.speed.x += SPEED,
                 _ => ()
             }
         }
@@ -76,10 +81,10 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        posX: 0.0,
-        posY: 0.0,
-        speedX: 0.0,
-        speedY: 0.0
+        position: Position { x: 0.0, y: 0.0 },
+        speed: Position { x: 0.0, y: 0.0 },
+        map: load_map(),
+        character: load_character()
     };
 
     let mut events = Events::new(EventSettings::new());
@@ -96,4 +101,14 @@ fn main() {
             app.change_direction(&args)
         }
     }
+}
+
+fn load_map() -> Texture{
+    let path = Path::new("./assets/map.jpg");
+    Texture::from_path(path, &TextureSettings::new()).unwrap()   
+}
+
+fn load_character() -> Texture {
+    let path = Path::new("./assets/rust.png");
+    Texture::from_path(path, &TextureSettings::new()).unwrap()
 }
